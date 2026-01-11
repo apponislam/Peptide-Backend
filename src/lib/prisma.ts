f511cce250551d8837a62ceda67ef9209b169792
Appon Islam <appon2003@gmail.com>
2026-01-12 00:05:29 +0600

    Remove old project files

--- src/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "../../generated/prisma/client";
-
-const prisma = new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    accelerateUrl: undefined,
-    adapter: undefined,
-} as any);
-
-export default prisma;
