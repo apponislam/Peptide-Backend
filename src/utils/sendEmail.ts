e87578dd5dd9e6b0c16adf6d42d815d5ceb4d99b
Appon Islam <appon2003@gmail.com>
2026-02-01 03:12:30 +0600

    send email updated

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,34 +1,49 @@
+// import { Resend } from "resend";
+// import config from "../config";
+
+// const resend = new Resend(config.resend_api_key);
+
+// async function sendEmail() {
+//     const fromEmail = config.resend_email!;
+
+//     try {
+//         const response = await resend.emails.send({
+//             from: fromEmail,
+//             to: ["apponislamdev@gmail.com"],
+//             subject: "Test Email",
+//             html: "<p>This is a test email.</p>",
+//         });
+
+//         console.log("Email sent:", response);
+//     } catch (error) {
+//         console.error("Error:", error);
+//     }
+// }
+
+// export default sendEmail;
+
 import { Resend } from "resend";
 import config from "../config";
 
 const resend = new Resend(config.resend_api_key);
 
-async function sendEmail() {
+export async function sendEmail(to: string | string[], subject: string, html: string) {
     const fromEmail = config.resend_email!;
 
     try {
         const response = await resend.emails.send({
             from: fromEmail,
-            to: ["apponislamdev@gmail.com"],
-            subject: "Test Email",
-            html: "<p>This is a test email.</p>",
+            to: to,
+            subject: subject,
+            html: html,
         });
 
         console.log("Email sent:", response);
-    } catch (error) {
+        return { success: true, data: response };
+    } catch (error: any) {
         console.error("Error:", error);
+        return { success: false, error: error.message };
     }
 }
 
 export default sendEmail;
-
-// import { Resend } from "resend";
-
-// const resend = new Resend("adsfasdf");
-
-// resend.emails.send({
-//     from: "onboarding@resend.dev",
-//     to: "apponislamdev@gmail.com",
-//     subject: "Hello World",
-//     html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-// });
