76a6a9189dd241420fac4493b32416e9dc793e3d
Appon Islam <appon2003@gmail.com>
2026-02-03 00:43:26 +0600

    send email commented lines removed

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,27 +1,3 @@
-// import { Resend } from "resend";
-// import config from "../config";
-
-// const resend = new Resend(config.resend_api_key);
-
-// async function sendEmail() {
-//     const fromEmail = config.resend_email!;
-
-//     try {
-//         const response = await resend.emails.send({
-//             from: fromEmail,
-//             to: ["apponislamdev@gmail.com"],
-//             subject: "Test Email",
-//             html: "<p>This is a test email.</p>",
-//         });
-
-//         console.log("Email sent:", response);
-//     } catch (error) {
-//         console.error("Error:", error);
-//     }
-// }
-
-// export default sendEmail;
-
 import { Resend } from "resend";
 import config from "../config";
 
